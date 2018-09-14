dbbaa20c59772d1c52f9cc9a0b434c946ebe3c54
Elyes Turki <elyestu@gmail.com>
2018-09-14 17:29:32 +0100

    add gulp tasks compress:css compress:js

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,83 @@ var babelify = require('babelify');
 var del = require('del');
 var source = require("vinyl-source-stream");
 var buffer = require("vinyl-buffer");
+var stripCssComments = require('gulp-strip-css-comments');
 var browserSync = require('browser-sync').create();
 
+var config = {
+    cssFiles: {
+        source: 'css/**/*.css',
+        dest: 'dist/css',
+        concatName: 'app.min.css'
+    },
+    jsFiles: {
+        source: 'js/**/*.js',
+        dest: 'dist/js',
+        concatName: 'app.js'
+    },
+    htmlFiles: {
+        source: '*.html',
+        dest: 'dist/',
+    },
+    pluginsCssFiles: {
+        source: 'css/**/*.css',
+        dest: 'dist/css',
+        concatName: 'plugins.css'
+    },
+    pluginsJsFiles: {
+        source: 'js/**/*.js',
+        dest: 'dist/js',
+        concatName: 'plugins.js'
+    },
+};
+
+//gulp default test
 gulp.task('default', defaultTask);
 
 function defaultTask(done) {
     console.log("default fonctionne bien !!!");
     done();
 }
+
+// task js compression
+gulp.task('compress:js', function() {
+    return gulp.src(config.jsFiles.source)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat(config.jsFiles.concatName))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.jsFiles.dest))
+        .on('finish', function() {
+            console.log("compress jsFiles files OK !!!");
+        });
+});
+
+// task js plugins compression
+gulp.task('compress:js-plugins', function() {
+    return gulp.src(config.pluginsJsFiles.source)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat(config.pluginsJsFiles.concatName))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.pluginsJsFiles.dest))
+        .on('finish', function() {
+            console.log("compress pluginsJsFiles files OK !!!");
+        });
+});
+
+// task css compression
+gulp.task('compress:css', function() {
+    return gulp.src(config.cssFiles.source)
+        .pipe(stripCssComments())
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS({
+            relativeTo: config.cssFiles.dest,
+            target: config.cssFiles.dest,
+        }))
+        .pipe(concat(config.cssFiles.concatName))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.cssFiles.dest))
+        .on('finish', function() {
+            console.log("compress cssFiles files OK !!!");
+        });
+});
--- readme.txt
+++ readme.txt
@@ -12,6 +12,7 @@
 >touch gulpfile.js
 >npm install gulp-uglify gulp-clean-css gulp-concat gulp-connect vinyl-source-stream gulp-htmlmin vinyl-buffer gulp-rename gulp-sourcemaps gulp-autoprefixer del browser-sync browserify babelify --save-dev
 >npm install --save-dev gulp-babel @babel/core @babel/preset-env --save-dev
+>npm install --save-dev gulp-strip-css-comments
 
 >npm audit (optionnal and to igniore)
 
